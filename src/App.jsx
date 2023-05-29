2172fec93fc9aeabb717e86b56a902cea9dcadf4
kennydl <kennydl.dev@gmail.com>
2023-05-29 12:06:13 +0200

    AutoGpt: Added components for About Me, Experience, and Technology

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react';
 import './App.css';
+import AboutMe from './components/AboutMe';
+import Experience from './components/Experience';
+import Technology from './components/Technology';
 
 function App() {
-  const [showHello, setShowHello] = useState(false);
-
   return (
     <div className='container'>
-      {showHello ? <div><div>Hello from AI</div><div>{'Did you know that the first computer mouse was made of wood?'}</div></div> : <button style={{backgroundColor: 'violet'}} onClick={() => setShowHello(true)}>Click me</button>}
+      <AboutMe />
+      <Experience />
+      <Technology />
     </div>
   );
 }
