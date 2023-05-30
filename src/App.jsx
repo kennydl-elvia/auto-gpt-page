bc2c469e2913836fc03ded3355eb588c4d10bc32
kennydl <kennydl.dev@gmail.com>
2023-05-30 21:05:36 +0200

    Revert stupid implementation by AI "AutoGpt: Added ConfettiButton component to App component"
    This reverts commit 43459ffc881dbac8cd78a6d6af48c3261ce40624.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 import AboutMe from './components/AboutMe';
 import Experience from './components/Experience';
 import Technology from './components/Technology';
-import ConfettiButton from './components/ConfettiButton';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <AboutMe />
       <Experience />
       <Technology />
-      <ConfettiButton />
     </div>
   );
 }
